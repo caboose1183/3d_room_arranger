de33a835b580fde388b860dbf62071ae7d864073
Sidney Pang <sidneypang@hotmail.co.uk>
2023-07-02 21:50:13 -0400

    set up pages, canvas, and components folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+
+import Canvas from "./canvas";
+import RoomEdit from "./pages/RoomEdit";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
-    
-      <div>
-        
-      </div>
-      
-    
-  )
+    <main className=" relative w-full h-screen overflow-hidden transition-all ease-in">
+      {/* <h1 className="text-black leading-[10rem] xl:leading-[11rem] text-[4rem] xl:text-[6rem]">
+        Let's Make You Dream Room
+      </h1> */}
+
+      <Home />
+      <Canvas />
+      <RoomEdit />
+    </main>
+  );
 }
 
-export default App
+export default App;
