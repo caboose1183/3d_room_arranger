0d7934e75cd1b7fd787213bb22139fc12bfbfa4a
Sidney Pang <sidneypang@hotmail.co.uk>
2023-07-11 01:29:00 -0400

    added walls and floor

--- client/src/canvas/Room.jsx
+++ client/src/canvas/Room.jsx
@@ -1,18 +1,15 @@
 import React, { useRef, useState } from "react";
 
+import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
-import {
-  Decal,
-  TransformControls,
-  useTexture,
-  Box,
-} from "@react-three/drei";
+import { Decal, TransformControls, useTexture, Box } from "@react-three/drei";
 
 import { useSnapshot } from "valtio";
 import state from "../store";
 
 import Desk from "./furniture/Desk";
 import Couch from "./furniture/Couch";
+import Floor from "./floor/Floor";
 
 const Room = () => {
   const snap = useSnapshot(state);
@@ -39,10 +36,99 @@ const Room = () => {
 
   // to change model position, apply position to the transform controls
 
-  const stateString = JSON.stringify(snap);
+  // geometry and material for walls
+  const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
+  const floor1Material = new THREE.MeshStandardMaterial({
+    color: "#2e302f",
+    metalness: 0,
+    roughness: 1,
+  });
+
+  const stateString = JSON.stringify(snap);
   return (
     <group key={stateString}>
+      {/* top floors */}
+      <Floor
+        scaleModifier={1}
+        position={[-3.85, 0, 0]}
+        rotation-y={Math.PI * 1}
+      />
+      <Floor scaleModifier={1} position={[0, 0, 0]} rotation-y={Math.PI * 1} />
+      <Floor
+        scaleModifier={1}
+        position={[3.9, 0, 0]}
+        rotation-y={Math.PI * 1}
+      />
+      <Floor
+        scaleModifier={1}
+        position={[7.8, 0, 0]}
+        rotation-y={Math.PI * 1}
+      />
+
+      {/* middle floors */}
+      <Floor
+        scaleModifier={1}
+        position={[-3.86, 0, 3.63]}
+        rotation-y={Math.PI * 1}
+      />
+      <Floor
+        scaleModifier={1}
+        position={[0, 0, 3.63]}
+        rotation-y={Math.PI * 1}
+      />
+      <Floor
+        scaleModifier={1}
+        position={[3.94, 0, 3.63]}
+        rotation-y={Math.PI * 1}
+      />
+      <Floor
+        scaleModifier={1}
+        position={[7.88, 0, 3.63]}
+        rotation-y={Math.PI * 1}
+      />
+
+      {/* bottom floors */}
+
+      <Floor
+        scaleModifier={1}
+        position={[-3.86, 0, 7.25]}
+        rotation-y={Math.PI * 1}
+      />
+      <Floor
+        scaleModifier={1}
+        position={[-0.1, 0, 7.25]}
+        rotation-y={Math.PI * 1}
+      />
+      <Floor
+        scaleModifier={1}
+        position={[3.84, 0, 7.25]}
+        rotation-y={Math.PI * 1}
+      />
+      <Floor
+        scaleModifier={1}
+        position={[7.78, 0, 7.25]}
+        rotation-y={Math.PI * 1}
+      />
+
+      {/* left wall */}
+      <mesh
+        position={[-5.85, 2.5, 3.62]}
+        geometry={boxGeometry}
+        material={floor1Material}
+        scale={[0.05, 5, 10.9]}
+        receiveShadow
+      />
+
+      {/* back wall */}
+      <mesh
+        position={[2, 2.5, -1.8]}
+        geometry={boxGeometry}
+        material={floor1Material}
+        scale={[15.8, 5, 0.05]}
+        receiveShadow
+      />
+
       {/* desk */}
       <TransformControls
         enabled={selectedModel === modelRefs.current[0]}
